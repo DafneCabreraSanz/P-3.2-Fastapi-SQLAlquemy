72d08bb206a5bd7caa25c16f8dbb7b55d1abf25d
DafneCabreraSanz <alumno.712343@ies-azarquiel.es>
2026-01-27 08:42:48 +0100

    cors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from database.database import Base, engine
 from routes import cantantes, albumes
@@ -13,3 +15,14 @@ app.include_router(albumes.router)
 @app.get("/")
 def root():
     return {"message": "API de Cantantes y Álbumes con FastAPI y SQLite"}
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=[
+"http://localhost:5173", # React (Vite)
+"http://localhost:8000" # React (Create React App)
+],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
